847e176f695506994d035fd04f050fff94d72883
pretrehr <raphael.pretre@orange.fr>
2019-10-01 18:48:06 +0200

    menu deroulant cree nouvelle page

--- app/src/main/java/com/example/android/bettingapp/GainActivity.java
+++ app/src/main/java/com/example/android/bettingapp/GainActivity.java
@@ -14,7 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class GainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
