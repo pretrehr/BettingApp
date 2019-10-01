22cfd5f0a1575cecb888b26149bae9e027122392
pretrehr <raphael.pretre@orange.fr>
2019-10-01 19:37:40 +0200

    link MainActivity - GainActivity

--- app/src/main/java/com/example/android/bettingapp/GainActivity.java
+++ app/src/main/java/com/example/android/bettingapp/GainActivity.java
@@ -20,18 +20,9 @@ public class GainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gain);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
