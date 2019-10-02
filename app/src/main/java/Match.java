98b4a1e92ff9608a7a63fabe9e244abad2969e23
pretrehr <raphael.pretre@orange.fr>
2019-10-02 20:48:23 +0200

    Interface simpliste calcul du gain

---
+++ app/src/main/java/Match.java
@@ -0,0 +1,20 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * Created by Raphael on 02/10/2019.
+ */
+
+public class Match {
+
+    private Odds odds;
+    private LocalDateTime date;
+    private ArrayList<String> opponents;
+
+
+    public Match(ArrayList<String> opponents, Odds odds, LocalDateTime date) {
+        this.opponents = opponents;
+        this.odds = odds;
+        this.date = date;
+    }
+}
